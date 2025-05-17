641ab007608383786a2fb23ccf46787acf28b3d9
IT24100551 <it24100551@my.sliit.lk>
2025-05-17 17:25:27 +0530

    Update PaymentService.java

--- it24100551_PaymentComponent/payment_system/src/main/java/com/tourism/service/PaymentService.java
+++ it24100551_PaymentComponent/payment_system/src/main/java/com/tourism/service/PaymentService.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
 
-    private static final String FILE_PATH = "E:\\zp\\payment_system\\src\\main\\resources\\payments.txt";
     private static final String FILE_PATH = "C:\\Users\\USER\\Desktop\\zp\\payment_system\\src\\main\\resources\\payments.txt";
 
     public PaymentService() {
